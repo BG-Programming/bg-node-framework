4e2c7f8d926161639751caa4dc32599443a4186a
BGKim <embc32@hotmail.com>
2023-04-20 05:08:26 +0900

    update db setup script

--- _setup/setup.sh
+++ _setup/setup.sh
@@ -1,2 +1,4 @@
-sudo -u postgres psql -f _setup/init_role.sql
+# run with db admin
+PGPASSWORD="postgres_password" psql -U postgres -h localhost -f _setup/init_role.sql
+# run with project db admin
 PGPASSWORD=1234 psql -U bg -h localhost --dbname=bg_programming_skeleton  -f _setup/init_tables.sql
